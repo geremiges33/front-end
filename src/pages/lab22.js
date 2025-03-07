3e1f675d9223f2adf7c24ce73740a7e525b57746
Kemi9999 <temuujinbatchuluun9@gmail.com>
2025-03-07 13:25:26 +0800

    push

--- src/pages/lab22.js
+++ src/pages/lab22.js
@@ -4,6 +4,9 @@ export default function Home() {
   const [historicalToolsData, setHistoricalToolsData] = useState([]);
   const [clothesData, setClothesData] = useState([]);
   const [instrumentsData, setInstrumentsData] = useState([]);
+  const [ethnicGroupsData, setEthnicGroupsData] = useState([]);
+  const [provincesData, setProvincesData] = useState([]); 
+  const [historicalFiguresData, setHistoricalFiguresData] = useState([]); 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +24,18 @@ export default function Home() {
         const historicalToolsResponse = await fetch("https://mongol-api-rest.vercel.app/HistoricalTools");
         const historicalToolsResult = await historicalToolsResponse.json();
         setHistoricalToolsData(historicalToolsResult.historicalTools);
-      
+
+        const ethnicGroupsResponse = await fetch("https://mongol-api-rest.vercel.app/EthnicGroups");
+        const ethnicGroupsResult = await ethnicGroupsResponse.json();
+        setEthnicGroupsData(ethnicGroupsResult.ethnicGroups)
+
+        const provincesResponse = await fetch("https://mongol-api-rest.vercel.app/Provinces");
+        const provincesResult = await provincesResponse.json();
+        setProvincesData(provincesResult.provinces)     
+
+        const historicalFiguresResponse = await fetch("https://mongol-api-rest.vercel.app/HistoricalFigures");
+        const historicalFiguresResult = await historicalFiguresResponse.json();
+        setProvincesData(historicalFiguresResult.historicalFigures)   
 
 
       } catch (error) {
@@ -108,6 +122,7 @@ export default function Home() {
             {item.materials && (
               <p className="text-sm text-gray-600 mt-2 flex ps-4">
                 <h1>Materials:</h1> {item.materials}
+
               </p>
             )}
 
@@ -119,6 +134,98 @@ export default function Home() {
           </div>
         ))}
       </div>
+      <h1 className="text-3xl font-semibold text-center mb-8 mt-12">EthnicGroups</h1>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+  {ethnicGroupsData.map((item, index) => (
+    <div key={index} className="bg-gray-100 rounded-lg shadow-lg">
+      {item.images && (
+        <img
+          src={item.images}
+          alt={item.name}
+          className="w-full h-64 object-cover rounded-lg mb-4"
+        />
+      )}
+      <h3 className="text-lg font-md text-gray-800 ps-4">{item.name}</h3>
+      <p className="text-sm text-gray-600 mt-2 ps-4">{item.description}</p>
+
+      {item.materials && (
+        <p className="text-sm text-gray-600 mt-2 flex ps-4">
+          <h1>Materials:</h1> {item.materials}
+
+        </p>
+      )}
+
+      {item.timePeriod && (
+        <p className="text-sm text-gray-600 flex ps-4">
+          <h1>Time Period:</h1> {item.timePeriod}
+        </p>
+      )}
     </div>
+  ))}
+</div>
+
+<h1 className="text-3xl font-semibold text-center mb-8 mt-12">Provinces</h1>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+  {provincesData.map((item, index) => (
+    <div key={index} className="bg-gray-100 rounded-lg shadow-lg">
+      {item.images && (
+        <img
+          src={item.images}
+          alt={item.name}
+          className="w-full h-64 object-cover rounded-lg mb-4"
+        />
+      )}
+      <h3 className="text-lg font-md text-gray-800 ps-4">{item.name}</h3>
+      <p className="text-sm text-gray-600 mt-2 ps-4">{item.description}</p>
+
+      {item.materials && (
+        <p className="text-sm text-gray-600 mt-2 flex ps-4">
+          <h1>Materials:</h1> {item.materials}
+
+        </p>
+      )}
+
+      {item.timePeriod && (
+        <p className="text-sm text-gray-600 flex ps-4">
+          <h1>Time Period:</h1> {item.timePeriod}
+        </p>
+      )}
+    </div>
+  ))}
+</div>
+
+<h1 className="text-3xl font-semibold text-center mb-8 mt-12">historicalFigures</h1>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+  {historicalFiguresData.map((item, index) => (
+    <div key={index} className="bg-gray-100 rounded-lg shadow-lg">
+      {item.images && (
+        <img
+          src={item.images}
+          alt={item.name}
+          className="w-full h-64 object-cover rounded-lg mb-4"
+        />
+      )}
+      <h3 className="text-lg font-md text-gray-800 ps-4">{item.name}</h3>
+      <p className="text-sm text-gray-600 mt-2 ps-4">{item.description}</p>
+
+      {item.materials && (
+        <p className="text-sm text-gray-600 mt-2 flex ps-4">
+          <h1>Materials:</h1> {item.materials}
+
+        </p>
+      )}
+
+      {item.timePeriod && (
+        <p className="text-sm text-gray-600 flex ps-4">
+          <h1>Time Period:</h1> {item.timePeriod}
+        </p>
+      )}
+    </div>
+  ))}
+</div>
+
+    </div>
+
+
   );
 }
